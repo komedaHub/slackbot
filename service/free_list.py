3f27f10229eb4e2a1e06b78ccaa731dbd0f095bd
yonedakazuki <wearegolden317@gmail.com>
2019-08-06 23:49:22 +0900

    aaa

---
+++ service/free_list.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+class FreeList:
+    @classmethod
+    def add(cls, arg1):
+        print(arg1)
+
+    def change():
+        pass
+
+    def delete():
+        pass
+
+    def read():
+        pass
